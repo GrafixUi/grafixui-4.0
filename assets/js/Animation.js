3a3349349689da07b9491b8c3290fbd7fd87c407
sanjaidebuger <nsksanjai@gmail.com>
2023-01-11 13:11:52 +0530

    Landing page

---
+++ assets/js/Animation.js
@@ -0,0 +1,9 @@
+let home = document.getElementById("home");
+let service = document.getElementById("service");
+
+window.addEventListener('scroll',function() {
+var value = window.scrollY;
+
+
+
+})
